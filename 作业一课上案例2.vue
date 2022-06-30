d63985820aec070e9f08310810e28f328196645b
tukasa <497116162@qq.com>
2022-06-30 19:17:36 +0800

    完成了课上案例2

---
+++ 作业一课上案例2.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(item,index) in arr" :key="index">{{item}}</li>
+    </ul>
+    <button @click="fn">点击换位</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      arr: ["帅哥", "美女", "程序猿"],
+    }
+  },
+  methods: {
+    fn() {
+      this.arr.push(this.arr.shift())
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
