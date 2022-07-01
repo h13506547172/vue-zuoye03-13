68a65a453e401995c1ac0ae9bcd789e125833bda
tukasa <497116162@qq.com>
2022-07-01 20:33:47 +0800

    完成了求和作业

---
+++ 求和作业.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="item in arr" :key="item">
+      <input type="checkbox" name="" id="" v-model="checkArr" :value="item">
+      {{item}}
+      </li>
+    </ul>
+    <div>总和{{sum}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      arr: [9, 15, 19, 25, 29, 31, 48, 57, 62, 79, 87],
+      checkArr: []
+    }
+  },
+  computed: {
+    sum() {
+     return this.checkArr.reduce((sum, item) => {
+       return sum += item
+      },0)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
