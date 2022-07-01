d6943f7e98ef80f14da4ef6409f4ce8984c22b45
tukasa <497116162@qq.com>
2022-07-01 21:26:50 +0800

    完成了过滤器案例

---
+++ 过滤器案例.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <p>{{num}}</p>
+    <p>{{num | disNum | disNum}}</p>
+    <p>{{num  | disNum }}</p>
+    <p>{{data | disData}}</p>
+    <p>{{data | disData| disData}}</p>
+
+  </div>
+</template>
+
+<script>
+import moment from "moment";
+export default {
+  data () {
+    return {
+      num: 18222547,
+      data: 1636450540055
+    }
+  },
+  filters: {
+    disNum(val) {
+      if (typeof val == 'string') {
+        return val.substr(0, val.length-1)/10 +'万'
+      }
+      return val/1000 +'千'
+    },
+    disData(val) {
+      if (typeof val == 'string') {
+        return val.replace(/-/g ,'/')
+      }
+    //   const data = new Date(val)
+    //  return `${data.getFullYear()}-${data.getMonth()+1}-${data.getDate()}`
+    return moment(val).format('YYYY-MM-DD')
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
