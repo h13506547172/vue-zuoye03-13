af79b86107560d557971063f160d94efe0bfc5f6
tukasa <497116162@qq.com>
2022-07-07 18:07:56 +0800

    完成了指令实现按钮的隐藏

---
+++ 作业权限指令.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <button v-for="(item,index) in arr" :key="index" v-isShow="item">只有有权限的看的见</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      //0-3可显示
+      arr: [
+        { show: 0 },
+        { show: 1 },
+        { show: 2 },
+        { show: 3 },
+        { show: 4 },
+        { show: 5 },
+      ],
+    };
+  },
+  directives: {
+    isShow: {
+      inserted(el, binding) {
+        // console.log(el);
+        // console.log(binding.value);
+        if (binding.value.show == 0 || binding.value.show == 1 || binding.value.show == 2) {
+          el.id = binding.value.show
+          const rem = document.getElementById(el.id)
+          // console.log(rem);
+          rem.remove()
+        }
+        
+      }
+    }
+  }
+};
+</script>
+
+<style></style>
